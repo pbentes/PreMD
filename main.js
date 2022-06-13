e83647e23dd49a8ae93003f1585d3be5fa0df3d5
Pedro Gomes <pedrogomes2000.3@gmail.com>
2022-06-13 02:14:23 +0100

    Initial Commit

---
+++ main.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const hljs = require('highlight.js/lib/common');
+
+var defaults = {
+    html: false,
+    xhtmlOut: false,
+    breaks: false,
+    langPrefix: 'language-',
+    linkify: true,
+    typographer: true,
+    _highlight: true,
+    _strict: false,
+    _view: 'html'
+};
+
+defaults.highlight = function (str, lang) {
+    var esc = md.utils.escapeHtml;
+    if (lang && hljs.getLanguage(lang)) {
+        try {
+            return '<pre class="hljs"><code>' + hljs.highlight(str, { language: lang, ignoreIllegals: true }).value + '</code></pre>';
+        } 
+        catch (err) {
+            console.log(err);
+        }
+    } else {
+        return '<pre class="hljs"><code>' + esc(str) + '</code></pre>';
+    }
+};
+
+const md = require('markdown-it')(defaults);
+
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
+    if (err) {
+        return console.log(err);
+    }
+
+    let result = md.render(data);
+
+    fs.writeFile(process.argv[3], result, 'utf8', (err) => {
+        if (err) {
+            return console.log(err);
+        }
+    })
+});
---
+++ test.md
@@ -0,0 +1,15 @@
+# test
+
+This is a test
+
+Lorem.
+
+```C
+
+#include <stdio.h>
+
+int main(void) {
+    return 0;
+}
+
+```
